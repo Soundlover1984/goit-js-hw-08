85596f4bd6c6d84f01d5cf24b08010f80c90a67e
Soundlover1984 <denys.kovtun@gmail.com>
2023-03-30 22:53:01 +0300

    gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryBox = document.querySelector('.gallery');
+const markupItems = galleryItemsCreate(galleryItems);
+
+function galleryItemsCreate(items) {
+    return items.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+     <a class="gallery__link" href="${original}"><img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    /></a></div>`;
+    }).join('')
+}
+
+galleryBox.insertAdjacentHTML("beforeend", markupItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+ 
 console.log(galleryItems);
+
+
