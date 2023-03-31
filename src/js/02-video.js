87983cb5a6194e32a10568f3008b51f1cb520240
Soundlover1984 <denys.kovtun@gmail.com>
2023-03-31 21:33:02 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,26 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
 const CT_KEY = 'videoplayer-current-time';
 
-const onPlay = function (data) {
-  localStorage.setItem(CT_KEY, data.seconds);
+const onPlay = ({ seconds }) => {
+  localStorage.setItem(CT_KEY, seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
-player.setCurrentTime(JSON.parse(localStorage.getItem(CT_KEY)) || 0);
+const currentVideoTime = JSON.parse(localStorage.getItem(CT_KEY));
+
+player
+  .setCurrentTime(currentVideoTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
 
