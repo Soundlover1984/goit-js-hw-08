3ce7f95923d5364fcacaf49942b9098ea9b9f98f
Denys Kovtun <denys.kovtun@gmail.com>
2023-03-31 18:13:47 +0300

    video set

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CT_KEY = 'videoplayer-current-time';
+
+const onPlay = function (event) {
+  localStorage.setItem(CT_KEY, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem(CT_KEY)) || 0);
 
