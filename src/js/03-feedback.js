83f07502430c127c601ccb1e638c9453a42dad0b
Soundlover1984 <denys.kovtun@gmail.com>
2023-03-31 23:35:07 +0300

    feedback start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
 
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector('.feedback-form');
+const FFS_KEY = "feedback-form-state";
+let data = {};
+formEl.addEventListener('input', throttle(onInput, 500));
+
+function onInput (event) {
+    data[event.target.name] = event.target.value;
+    localStorage.setItem(FFS_KEY, JSON.stringify(data))
+}
+
+if (localStorage.getItem(FFS_KEY)) {
+    const {email, message} = JSON.parse(localStorage.getItem(FFS_KEY));
+    formEl.email.value = email;
+    formEl.message.value = message;
+    data.email = email;
+    data.message = message;
+}
+
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(data);
+    if (formEl.email.value === '' || formEl.message.value === '') {
+    return alert('Будь ласка, заповніть всі поля!');}
+    localStorage.removeItem(FFS_KEY);
+    formEl.reset()
+}
